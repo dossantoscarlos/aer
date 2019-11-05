47ea2ba1b72f85c7f6555bd7e50624783039ac51
dossantoscarlos <anjoazrael10@gmail.com>
2019-11-05 13:40:49 -0300

    corrigindo nomeclaturas

---
+++ resources/views/config.blade.php
@@ -0,0 +1,143 @@
+@extends('cpanel.layout')
+@section('content')
+
+<link rel="stylesheet" href="{{ asset('css/bulma/css/bulma.min.css') }}">
+<div class="container column is-4">
+	<div class="title is-primary">
+		<h3 class="title has-text-centered">Cadastre seu usuario</h3>
+	</div>
+	<hr/>
+	<form method="post" action="{{ route('configPost') }}">
+	<div class="field">
+		@csrf
+			<p class="control">
+				<label class="label">Nome de usuario</label>
+				<input type="text" id="usuario" name="usuario" class="input is-large" placeholder="nome de usuario" />
+			</p>
+			<p class="control">
+				<label class="label">Nivel</label>
+				<div class="select is-large">
+					<select id="nivel" name='nivel'>
+						<option value="1">Administrador</option>
+					</select>
+				</div>
+			</p>
+		</div>
+		<div class="field">
+			<p class="control">
+				<label class="label">Senha para conectar</label>
+				<input type="password" id="password" name="password"
+				class='input is-large' placeholder="******" />
+			</p>
+			<p class="control">
+				<label class="label">Confirma senha</label>
+				<input type="password" class="input is-large" id="confpass" placeholder="******" />
+			</p>
+		</div>
+		<div class="field tile column is-7">
+			<p class="control">
+				<button type='button' id="btnsalvar" class="button is-success is-large" >Salvar</button>
+			</p>
+		</div>
+		</form>
+</div>
+
+<!--modal sucesso-->
+<div id="modal-sucesso" class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <article class="message is-success">
+  <div class="message-header">
+    <p>Sucesso</p>
+    <button id='btnclosesuccess' class="delete" aria-label="delete"></button>
+  </div>
+  <div class="message-body"></div>
+</article>
+</div>
+</div>
+
+<!-- modal error -->
+<div id="modal-error" class="modal">
+	<div class="modal-background"></div>
+	<div class="modal-content">
+	  <article class="message is-danger">
+	<div class="message-header">
+	  <p>Sucesso</p>
+	  <button id='btnclosedelete' class="delete" aria-label="delete"></button>
+	</div>
+	<div class="message-body"></div>
+  </article>
+  </div>
+  </div>
+
+<script type="text/javascript">
+
+	$(document).ready(function (){
+
+		$('#btnclosesuccess').click(function(){
+			$('#modal-sucesso').removeClass(' is-active is-clipped')
+			location.href="{{ route('login') }}";
+		})
+		$('#btnclosedelete').click(
+			function(){
+				$('#modal-error').removeClass(' is-active is-clipped')
+			}
+		)
+
+		$("#btnsalvar").on('click', function () {
+
+			const modalError = (msg) => {
+				$('#modal-error').addClass((index) => {
+					return index + ' is-active is-clipped'
+				});
+				$('.message-body').text(msg)
+			};
+
+			const modalSuccess = (msg) => {
+				$('#modal-sucesso').addClass(function(index){
+					return index + ' is-active is-clipped'
+				});
+				$('.message-body').text(msg)
+			};
+
+			let validaSenha = (pass,confpass) => {
+				if ($('#usuario').val().trim()==""){
+					modalError("Campo usuario esta em branco")
+					return false
+				}else if (pass.trim() == "" || confpass.trim() ==""  )
+				{
+					modalError("Campo senha ou confirma senha esta em branco")
+					return false
+				}else if (pass != confpass ){
+					modalError("Campo senha e confirma senha nao sao iguais")
+					console.log(pass)
+					return false
+				}else{
+					return true
+				}
+			}
+
+			if(validaSenha($("#password").val(),$("#confpass").val()))
+			{		
+				var  caminho = $('form').attr('action');
+				$.ajax({
+					method : "POST",
+					url: caminho,
+					data : {
+						password: $('#password').val(),
+						usuario : $('#usuario').val(),
+						nivel : $('#nivel').val(),
+					},
+					success:function (response) {
+						modalSuccess(response.msg)
+						console.log(response)
+					},
+					fail: function (resp,status){
+						modalError(resp)
+					}
+				});
+			}
+		})
+	});
+</script>
+@endsection
---
+++ resources/views/cpanel/groups.blade.php
@@ -0,0 +1,61 @@
+@extends('cpanel.layout')
+@section('content')
+<div class="hero is-light">
+	<div class="hero-body">
+		<div class="is-fluid">
+			<h1 class="title">
+				Cadrastamento de Grupos
+			</h1>
+			<h2 class="subtitle">
+				Digite o nome do grupo de usuario
+			</h2>
+		</div>
+	</div>
+</div>
+<div id="divProdutoBase" class="column is-offset-1 tile is-10 "style="display:none;">
+	<input type="text" class='input is-medium control is-6' name="produto[]" placeholder="Nivel de Grupo" />
+	<span> &nbsp;</span>
+	<button type="button" class="button is-danger is-medium is-6" onclick="produtoList.remove(this.parentNode)">Remover</button>
+</div>
+<form class="field">
+	<div id="divProdutoList"></div>
+	<p>&nbsp;</p>
+	<div class="field is-grouped is-grouped-centered">
+		<div class="control">
+			<button type="button" id='btngravar'class="button is-medium is-info ">Gravar</button>
+		</div>
+		<div class="control"> 
+			<button type="button" id='btnplus' onclick="produtoList.insert()" class="button is-medium is-primary">Mais</button>
+		</div>
+
+	</div> 
+</div>
+</form>
+
+<script type="text/javascript">
+	var produtoList = {
+		'init': function()
+		{
+			this.divProdutoList = document.getElementById('divProdutoList');
+			this.divProdutoBase = document.getElementById('divProdutoBase');
+		},
+
+		'insert': function()
+		{
+			var newDiv = this.divProdutoBase.cloneNode(true);
+			newDiv.style.display = '';
+			console.log('newDiv => ', newDiv);
+			this.divProdutoList.appendChild(newDiv);
+		},
+
+		'remove': function(el)
+		{
+			el.parentNode.removeChild(el);
+		}
+	};
+	produtoList.init();
+</script>
+
+</div>
+<p>&nbsp;</p>
+@endsection
